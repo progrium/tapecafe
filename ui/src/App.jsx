3225a63ded87a6d8baa38e4681bcaf2e54fe6233
Jeff Lindsay <progrium@gmail.com>
2025-07-20 23:00:49 -0700

    ui: fix skipping pre-join

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -4,6 +4,7 @@ import VideoRoom from './VideoRoom'
 
 function App() {
   const [connectionData, setConnectionData] = useState(null)
+  const [shouldJoin, setShouldJoin] = useState(false)
 
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search)
@@ -17,15 +18,17 @@ function App() {
 
   const handleConnect = (url, token, displayName) => {
     setConnectionData({ url, token, displayName })
+    setShouldJoin(true)
   }
 
   const handleDisconnect = () => {
     setConnectionData(null)
+    setShouldJoin(false)
   }
 
   return (
     <div>
-      {!connectionData ? (
+      {!shouldJoin ? (
         <ConnectForm onConnect={handleConnect} />
       ) : (
         <VideoRoom 
